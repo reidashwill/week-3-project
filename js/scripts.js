9ac3ccffab2bdab0148d838e57c7384bcea6ae50
Reid Ashwill <reidashwill@gmail.com>
2020-04-03 11:37:05 -0700

    change foreach loop into a some loop to break out after returning true

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,17 @@
 function robotize(input){
   const outputs = ["Beep!","Boop!","Won't you be my neighbor?"]
   var newArray = input.split("")
-  newArray.forEach(myFunction);
+  newArray.some(myFunction);
   
   function myFunction(newArray){
-    if(newArray[0] >=1 && newArray[0] <=3){
-      console.log(outputs[parseInt(newArray[0])-1])
+    var outputArray = []
+    if(newArray >=1 && newArray <=3){
+      outputArray.push(outputs[newArray-1])
       debugger;
-      return outputs[parseInt(newArray[0])]
-    }
+      console.log(outputArray)
+      return true
+    };
   }
-
 };
 
 
@@ -22,6 +23,7 @@ $(document).ready(function(){
     event.preventDefault();
     var input = $("#userNumber").val();
     var result = robotize(input);
+    
     $("#formGroup")[0].reset();
     $(".outputField").append(" " + result);    
   });
