6a09ba1e3c2986ca02852aa12a9244576153b348
Reid Ashwill <reidashwill@gmail.com>
2020-04-03 08:59:32 -0700

    troubleshoot business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,27 @@
 function robotize(input){
   const outputs = ["Beep!"]
+
+  for(i=input; i<=outputs.length; i ++){
+    return outputs[input-1]
+    debugger;
+  }
 }
+
+
 
 
 
 $(document).ready(function(){
   $("form#formGroup").submit(function(event){
     event.preventDefault();
-    var input = $("#userNumber").val();
-    $(".outputField").append(" " + input);
-  });
-  debugger;
+    var input = parseInt($("#userNumber").val());
+    var result = robotize(input);
+    if(result){
+    $(".outputField").append(" " + result);
+    console.log(result)
+    debugger;
+  }
+  
+});
+  
 });
