986e5316983dfc43a28f0b35c54020cef71251b8
Reid Ashwill <reidashwill@gmail.com>
2020-04-03 08:34:35 -0700

    add array that contains one index, which is a string of the low priority change to be made

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
-(document).ready(function(){
+function robotize(input){
+  const outputs = ["Beep!"]
+}
+
+
+
+$(document).ready(function(){
   $("form#formGroup").submit(function(event){
     event.preventDefault();
-    
-    $(".outputField").text(result);
+    var input = $("#userNumber").val();
+    $(".outputField").append(" " + input);
   });
+  debugger;
 });
