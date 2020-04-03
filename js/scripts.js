f1b9c79d6c91bd186a9a2455e3737b93c8826e56
Reid Ashwill <reidashwill@gmail.com>
2020-04-03 13:59:05 -0700

    fix passing information back to UI logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,33 @@
 function robotize(input){
-  const outputs = ["Beep!","Boop!","Won't you be my neighbor?"]
+  var outputs = ["Beep!","Boop!","Won't you be my neighbor?"]
   var newArray = input.split("")
-  newArray.some(myFunction);
+  var outputArray = []
   
+  newArray.some(myFunction);
   function myFunction(newArray){
-    var outputArray = []
-    if(newArray === 3){
+      if(newArray.includes(3)){
       outputArray.push(outputs[2])
-      debugger;
-      return true
-    }else if(newArray ===2 ){
-      outputArray.push(outputs[1])
-      debugger;
-      return true
-    }else if(newArray === 1){
-      outputArray.push(outputs[0])
-      debugger;
-      return true
-    } 
-    console.log(outputArray)
-    return outputArray
+      
+    }
+    
   }
+  console.log(outputArray.join());
+  return (outputArray.join())
 };
-
-
-
 
 
 $(document).ready(function(){
   $("form#formGroup").submit(function(event){
     event.preventDefault();
     var input = $("#userNumber").val();
-    var result = robotize(input);
-    
+    var output = robotize(input);
     $("#formGroup")[0].reset();
-    $(".outputField").append(" " + result);    
+    $(".outputField").append(" " + output);    
   });
 });
+
+// function result(outputArray){
+//   console.log(outputArray);
+//   $("#formGroup")[0].reset();
+//   $(".outputField").append(" " + output);
+//   console.log(output);
