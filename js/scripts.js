1c8231893bf07a735b33c9779b0e793cf995c441
Reid Ashwill <reidashwill@gmail.com>
2020-04-03 09:11:19 -0700

    fix error in array index values in business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 function robotize(input){
   const outputs = ["Beep!"]
-
-  for(i=input; i<=outputs.length; i ++){
-    return outputs[input-1]
-    debugger;
-  }
+   if(input === 1){
+     console.log(outputs[0])
+     return outputs[0]
+   }
+  // for(i=input; i<=outputs.length; i ++){
+  //   return outputs[input-1]
+    
+  // }
 }
 
 
@@ -15,13 +18,11 @@ $(document).ready(function(){
   $("form#formGroup").submit(function(event){
     event.preventDefault();
     var input = parseInt($("#userNumber").val());
+    console.log("test")
     var result = robotize(input);
-    if(result){
-    $(".outputField").append(" " + result);
-    console.log(result)
+    console.log("test")
     debugger;
-  }
-  
-});
-  
+    $(".outputField").append(" " + result); 
+    
+  });
 });
