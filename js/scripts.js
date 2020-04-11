24bceb5478560a308baa32ed532b1527b5669737
Reid Ashwill <reidashwill@gmail.com>
2020-04-11 08:57:40 -0700

    add a spaces to items being pushed to array to make the end display easier to read by the end user

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,13 @@ function robotize(input){
     testNumber = i;
     
     if (i.toString().includes("3")) {
-      outputArray.push("Won't you be my neighbor?")
-    }else if (i.toString().includes("2")) {
-      outputArray.push("Boop!")
-    }else if (i.toString().includes("1")){
-      outputArray.push("Beep!")
+      outputArray.push(" Won't you be my neighbor?")
+    }else if (i.toString().includes(" 2")) {
+      outputArray.push(" Boop!")
+    }else if (i.toString().includes(" 1")){
+      outputArray.push(" Beep!")
     }else{
-      outputArray.push(i)
+      outputArray.push(" " + i)
     }
     console.log(outputArray)
   }
